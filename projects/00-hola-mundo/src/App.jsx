2dfb56e2070c5e10ec0c93b9a0da59b3d216ca57
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2024-03-25 15:20:18 -0600

    Adding using of hook 'useState'

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,15 +1,22 @@
+import { useState } from 'react'
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 export function App() {
+    // When you update the state of the parent component, the parent component propagates 
+    // the change downstream to the child component
+    // const [name, setName] = useState('sindresorhus')
     return (
         <section className='App'>
-            <TwitterFollowCard isFollowing userName='sindresorhus'>
+            {/* <TwitterFollowCard isFollowing userName={name}> */}
+            {/* <TwitterFollowCard isFollowing userName="sindresorhus" initialIsFollowing={true}> */}
+            <TwitterFollowCard userName="sindresorhus">
                 Kutra
             </TwitterFollowCard>
-            <TwitterFollowCard isFollowing={false} userName='suja'>
+            <TwitterFollowCard userName='suja'>
                 Suja
             </TwitterFollowCard>
+            {/* <button onClick={() => setName('Pedro') }>Change Name</button> */}
         </section>
     )
 }
--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react'
+
 //sindresorhus
 // Props should be inmutable you can assign the prop to a constant
 // the prop childre is a special prop that chan be used to inject an element or text in my component
-export function TwitterFollowCard({children, userName = 'unknow', name, isFollowing}) {
+//export function TwitterFollowCard({children, userName = 'unknow', initialIsFollowing}) {
+export function TwitterFollowCard({children, userName = 'unknow'}) {
+    // initiate valuses in a components only happens 1 time and if the parent component change the 
+    // state, the value is not propagated to the child.
+    const [isFollowing, setIsFollowing] = useState(false);
+    const handleClick = () => {
+        setIsFollowing(!isFollowing);
+    };
     console.log(isFollowing)
     const text = isFollowing ? 'Following' : 'Follow';
     const buttonClassName = isFollowing 
@@ -17,8 +26,9 @@ export function TwitterFollowCard({children, userName = 'unknow', name, isFollow
                 </div>
             </header>
             <aside>
-                <button className={buttonClassName}>
-                    {text}
+                <button className={buttonClassName} onClick={handleClick}>
+                    <span className="tw-followCard-text">{text}</span>
+                    <span className="tw-followCard-stopFollow">Stop following</span>
                 </button> 
             </aside>
         </article>
