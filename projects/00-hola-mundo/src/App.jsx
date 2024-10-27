fce88e1cf9d177d3bcf2b53f59d0d8f4a8572798
Oscar Bonilla Rodríguez <oscarbonillarodriguez@Oscars-MacBook-Air.local>
2024-10-27 11:47:09 -0600

    Configuring ESLint, Standar and to modify rules
    Modifying rules for gitignore
    
    Adding recomended extensions
    
    @stylistic/eslint-plugin-js

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,62 +1,60 @@
-import { useState } from 'react'
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
 
 const users = [
-    {
-        id: 1,
-        userName: 'sindresorhus',
-        name: 'Sindresorhus Tra',
-        isFollowing: true
-    },
-    {
-        id: 2,
-        userName: 'suja',
-        name: 'Suja Kin',
-        isFollowing: false
-    },
-    {
-        id: 3,
-        userName: 'peterSls',
-        name: 'Peter Salad',
-        isFollowing: true
-    },
-    {
-        id: 4,
-        userName: 'kalarGel1',
-        name: 'Kalar Geli',
-        isFollowing: false
-    }
-];
+  {
+    id: 1,
+    userName: 'sindresorhus',
+    name: 'Sindresorhus Tra',
+    isFollowing: true
+  },
+  {
+    id: 2,
+    userName: 'suja',
+    name: 'Suja Kin',
+    isFollowing: false
+  },
+  {
+    id: 3,
+    userName: 'peterSls',
+    name: 'Peter Salad',
+    isFollowing: true
+  },
+  {
+    id: 4,
+    userName: 'kalarGel1',
+    name: 'Kalar Geli',
+    isFollowing: false
+  }
+]
 
-// * When you create a component, the definition needs to be set in pascal case.this because react needs to be detect if the element 
+// * When you create a component, the definition needs to be set in pascal case.this because react needs to be detect if the element
 // is a component or html.
 export function App() {
-    // When you update the state of the parent component, the parent component propagates 
-    // the change downstream to the child component
-    // const [name, setName] = useState('sindresorhus')
-    return (
-        <section className='App'>
-            {/* <TwitterFollowCard isFollowing userName={name}> */}
-            {/* <TwitterFollowCard isFollowing userName="sindresorhus" initialIsFollowing={true}> */}
-            {/* <TwitterFollowCard userName="sindresorhus"> */}
-                {/* Kutra */}
-            {/* </TwitterFollowCard> */}
-            {/* <TwitterFollowCard userName='suja'> */}
-                {/* Suja */}
-            {/* </TwitterFollowCard> */}
-            {/* <button onClick={() => setName('Pedro') }>Change Name</button> */}
+  // When you update the state of the parent component, the parent component propagates
+  // the change downstream to the child component
+  // const [name, setName] = useState('sindresorhus')
+  return (
+    <section className='App'>
+      {/* <TwitterFollowCard isFollowing userName={name}> */}
+      {/* <TwitterFollowCard isFollowing userName="sindresorhus" initialIsFollowing={true}> */}
+      {/* <TwitterFollowCard userName="sindresorhus"> */}
+      {/* Kutra */}
+      {/* </TwitterFollowCard> */}
+      {/* <TwitterFollowCard userName='suja'> */}
+      {/* Suja */}
+      {/* </TwitterFollowCard> */}
+      {/* <button onClick={() => setName('Pedro') }>Change Name</button> */}
 
-            {
-                users.map(({id, userName, name, isFollowing}) => (
-                      <TwitterFollowCard 
-                        key={id}
-                        userName={userName} 
-                        initialIsFollowing={isFollowing}>
-                        {name}
-                      </TwitterFollowCard>  
-                ))
-            }
-        </section>
-    )
+      {users.map(({ id, userName, name, isFollowing }) => (
+        <TwitterFollowCard
+          key={id}
+          userName={userName}
+          initialIsFollowing={isFollowing}
+        >
+          {name}
+        </TwitterFollowCard>
+      ))}
+    </section>
+  )
 }
--- projects/01-tic-tac-toe/src/App.jsx
+++ projects/01-tic-tac-toe/src/App.jsx
@@ -1,57 +1,57 @@
-import { useState } from 'react';
-import './App.css';
-import confetti from 'canvas-confetti';
-import { Square } from './components/Square';
-import { TURNS } from './constants';
-import { checkWinner, checkEndGame } from './logc/board';
-import { WinnerModal } from './components/WinnerModal';
-import { MyBoard } from './components/Board';
-import { MyHeader } from './components/MyHeader';
-import { saveGameToStorage, restGameStorage } from './logc/storage';
+import { useState } from 'react'
+import './App.css'
+import confetti from 'canvas-confetti'
+import { Square } from './components/Square'
+import { TURNS } from './constants'
+import { checkWinner, checkEndGame } from './logc/board'
+import { WinnerModal } from './components/WinnerModal'
+import { MyBoard } from './components/Board'
+import { MyHeader } from './components/MyHeader'
+import { saveGameToStorage, restGameStorage } from './logc/storage'
 
 // App function
 function App() {
-  const [board, setBoard] = useState(() => {  
-    const boardFromLocalStorage = window.localStorage.getItem('board');
-    return boardFromLocalStorage ? JSON.parse(boardFromLocalStorage) : Array(9).fill(null);
-  });
+  const [board, setBoard] = useState(() => {
+    const boardFromLocalStorage = window.localStorage.getItem('board')
+    return boardFromLocalStorage
+      ? JSON.parse(boardFromLocalStorage)
+      : Array(9).fill(null)
+  })
 
   const [turn, setTurn] = useState(() => {
-    const turnFromStorage = window.localStorage.getItem('turn');
-    return turnFromStorage ?? TURNS.x;
-  });
-  
-  const [winner, setWinner] = useState(null);
-  
-  const resetGame = () => {
-    setBoard(Array(9).fill(null));
-    setTurn(TURNS.x);
-    setWinner(null);
+    const turnFromStorage = window.localStorage.getItem('turn')
+    return turnFromStorage ?? TURNS.x
+  })
 
-    restGameStorage();
+  const [winner, setWinner] = useState(null)
+
+  const resetGame = () => {
+    setBoard(Array(9).fill(null))
+    setTurn(TURNS.x)
+    setWinner(null)
+
+    restGameStorage()
   }
 
   const updateBoard = (index) => {
-    
-    if (board[index] || winner) return;
+    if (board[index] || winner) return
 
-    const newBoard = [...board];
-    newBoard[index] = turn;
-    setBoard(newBoard);
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
 
-    const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x;
-    setTurn(newTurn);
+    const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x
+    setTurn(newTurn)
 
-    const newWinner = checkWinner(newBoard);
+    const newWinner = checkWinner(newBoard)
 
-    saveGameToStorage({board: newBoard, turn: newTurn});
+    saveGameToStorage({ board: newBoard, turn: newTurn })
 
     if (newWinner) {
-      confetti();
-      setWinner(newWinner);
-    }
-    else if (checkEndGame(newBoard)) {
-      setWinner(false);
+      confetti()
+      setWinner(newWinner)
+    } else if (checkEndGame(newBoard)) {
+      setWinner(false)
     }
   }
 
@@ -69,7 +69,7 @@ function App() {
         <WinnerModal resetGame={resetGame} winner={winner} />
       </section>
     </main>
-  );
+  )
 }
 
 export default App
